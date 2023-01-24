a623e33a9795d36fa488858cd19d09891d075fdd
W Geng <gengwg@users.noreply.github.com>
2023-01-23 20:40:57 -0800

    Wrapper for etcdctl/etcd v3 query.

---
+++ etcd/etcdctl-wrapper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Wrapper for etcdctl/etcd v3 query.
+# Usage Example:
+# ./etcdctl-wrapper.sh endpoint status --cluster
+# ./etcdctl-wrapper.sh -w members list
+
+ETCD_CERT_FILE="/var/lib/kubernetes/etcd.pem"
+ETCD_KEY_FILE="/var/lib/kubernetes/etcd.key"
+ETCD_TRUSTED_CA_FILE="/etc/pki/ca-trust/tls-ca-bundle.pem"
+# List all endpoints
+ETCDRUN_ENDPOINTS="https://kubectrlplane101.example.com:2379,https://kubectrlplane102.example.com:2379,https://kubectrlplane103.example.com:2379"
+sudo ETCDCTL_API=3 etcdctl --cert=$ETCD_CERT_FILE --cacert=$ETCD_TRUSTED_CA_FILE --key=$ETCD_KEY_FILE  --endpoints=$ETCDRUN_ENDPOINTS $*
