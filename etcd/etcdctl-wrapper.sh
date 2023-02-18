8c6f4c6b903851e6c37a70f87438e3a08183a434
W Geng <gengwg@users.noreply.github.com>
2023-02-17 16:28:11 -0800

    Fix error rejected connection from EOF ServerName xxx
    Ref: https://github.com/etcd-io/etcd/issues/10587

--- etcd/etcdctl-wrapper.sh
+++ etcd/etcdctl-wrapper.sh
@@ -5,9 +5,16 @@
 # ./etcdctl-wrapper.sh endpoint status --cluster
 # ./etcdctl-wrapper.sh -w members list
 
+# Tested version:
+# $ etcdctl version
+# etcdctl version: 3.3.11
+$ API version: 3.3
+
+export ETCDCTL_API=3
+
 ETCD_CERT_FILE="/var/lib/kubernetes/etcd.pem"
 ETCD_KEY_FILE="/var/lib/kubernetes/etcd.key"
 ETCD_TRUSTED_CA_FILE="/etc/pki/ca-trust/tls-ca-bundle.pem"
 # List all endpoints
-ETCDRUN_ENDPOINTS="https://kubectrlplane101.example.com:2379,https://kubectrlplane102.example.com:2379,https://kubectrlplane103.example.com:2379"
+ETCDRUN_ENDPOINTS="https://kubectrlplane101.example.com:2379, https://kubectrlplane102.example.com:2379, https://kubectrlplane103.example.com:2379"
 sudo ETCDCTL_API=3 etcdctl --cert=$ETCD_CERT_FILE --cacert=$ETCD_TRUSTED_CA_FILE --key=$ETCD_KEY_FILE  --endpoints=$ETCDRUN_ENDPOINTS $*
